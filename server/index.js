45eccec2ac528dd2bf30c03b973602c863f5d3b4
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-10-06 09:05:48 -0400

    Set PORT to heroku app

--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ import quizRoutes from './routes/posts.js'
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// }
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (request, response) => {
+// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
@@ -22,7 +22,8 @@ app.use(cors());
 app.use('/users/create', quizRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://timhsu:WKtfS6puuVSOHhfJ@cluster0.w6qvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 'https://timhsu-mini-quiz-app.herokuapp.com/'
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
