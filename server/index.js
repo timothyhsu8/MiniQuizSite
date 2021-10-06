9fd18a6c0ca9936f06070891101d4c043dcc17cf
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-10-06 09:09:31 -0400

    Set PORT to 5000 again

--- server/index.js
+++ server/index.js
@@ -22,8 +22,7 @@ app.use(cors());
 app.use('/users/create', quizRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://timhsu:WKtfS6puuVSOHhfJ@cluster0.w6qvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-// const PORT = process.env.PORT || 5000;
-const PORT = 'https://timhsu-mini-quiz-app.herokuapp.com/'
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
